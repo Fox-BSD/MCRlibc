5c975a5548ef24ae547860ae8cc52c0f1c88775f
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2023-05-29 16:44:44 -0300

    [Feat]: added declaration of float.h

---
+++ include/float.h
@@ -0,0 +1,43 @@
+#ifndef _FLOAT_H
+#define _FLOAT_H
+
+#define FLT_RADIX       2
+#define FLT_ROUNDS      1
+
+#define FLT_MANT_DIG    6
+#define DBL_MANT_DIG    15
+#define LDBL_MANT_DIG   18
+
+#define FLT_DIG         6
+#define DBL_DIG         15
+#define LDBL_DIG        18
+
+#define FLT_MIN_EXP     (-37)
+#define DBL_MIN_EXP     (-307)
+#define LDBL_MIN_EXP    (-4931)
+
+#define FLT_MIN_10_EXP  (-37)
+#define DBL_MIN_10_EXP  (-307)
+#define LDBL_MIN_10_EXP (-4931)
+
+#define FLT_MAX_EXP     38
+#define DBL_MAX_EXP     308
+#define LDBL_MAX_EXP    4932
+
+#define FLT_MAX_10_EXP  38
+#define DBL_MAX_10_EXP  308
+#define LDBL_MAX_10_EXP 4932
+
+#define FLT_MAX         1.0e+37F
+#define DBL_MAX         1.0e+307
+#define LDBL_MAX        1.0e+4931L
+
+#define FLT_EPSILON     1.0e-6F
+#define DBL_EPSILON     1.0e-15
+#define LDBL_EPSILON    1.0e-18L
+
+#define FLT_MIN         1.0e-37F
+#define DBL_MIN         1.0e-307
+#define LDBL_MIN        1.0e-4931L
+
+#endif

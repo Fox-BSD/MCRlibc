bba99b611a571025ccfc82eeaafffa88901d6cb0
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2023-06-01 06:56:21 -0300

    [Ref]: Create a test target

--- xmake.lua
+++ xmake.lua
@@ -3,10 +3,14 @@ add_rules("mode.debug", "mode.release")
 set_toolset("cxx", "clang")
 set_toolset("ld", "clang++")
 
-target("MCRlibc")
+target("mcrlibc")
     set_kind("object")
     add_includedirs("include")
     add_files("lib/**.c")
     add_cflags("-nostdlib", "-nostdinc", "-ffreestanding")
 
-
+target("test")
+  set_kind("binary")
+  add_includedirs("test/include_test/", "include")
+  add_files("test/lib_test/*.c", "test/main.c")
+  add_cflags("-nostdlib", "-nostdinc", "-ffreestanding")
